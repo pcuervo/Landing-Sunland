e07896f7ddaab884f3e177755b0f1e7c11ec5a52
maichomper <maichomper@gmail.com>
2015-11-24 16:22:03 -0600

    agregar nuevo codigo analytics para forma

--- js/functions.js
+++ js/functions.js
@@ -17,8 +17,24 @@ function sendContactEmail( form ){
             $('.js-form-container').addClass('hidden');
             $('.js-name').text( jsonResponse.name );
             $('.js-thankyou').removeClass('hidden');
+            landingAnalytics();
 
         }
     );
 
 }// sendContactEmail
+
+/**
+* Analytics para landing page
+**/
+function landingAnalytics(){
+
+    dataLayer.push({
+        'event' : 'VirtualPageview',
+        'virtualPageURL':'/mensaje-lp',
+    });
+
+    var contactoIFrame = '<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WFG9VW" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>';
+    $('body').append( contactoIFrame );
+
+}// landingAnalytics
